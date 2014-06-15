b248c40d8efe6c0e42391941455177f6c3d09db8
shaan <shansree1@gmail.com>
2014-06-15 13:14:44 +0530

    initial version

---
+++ src/main/java/org/shan/mvc/controller/HelloController.java
@@ -0,0 +1,18 @@
+package org.shan.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/welcome")
+public class HelloController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(Model model) {
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		return "hello";
+ 
+	}
+}
